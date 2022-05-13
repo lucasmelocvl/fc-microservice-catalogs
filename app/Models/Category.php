ba86bb3fb577e92db52b5d3edbd729a9c5ba5f4f
lucasmelocvl <lucasmelocvl@gmail.com>
2022-05-12 22:26:26 -0300

    Changed primary key int to uuid id; Created Trait to use uuid

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Traits\Uuid;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -9,13 +10,15 @@ class Category extends Model
 {
     // Trait que adiciona um comportamento de deleção para realizar uma exclusão lógica, ao invés de física
     // Em toda a busca, que tiver data no deleted_at, não irá ser mostrada
-    use SoftDeletes;
+    use SoftDeletes, Uuid;
 
     protected $fillable = [
         'name',
         'description',
         'is_active'
     ];
-
     protected $dates = ['deleted_at'];
+    protected $casts = [
+        'id' => 'string'
+    ];
 }
