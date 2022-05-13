f3cfe644d72e818039c8922756a4063acad631d3
lucasmelocvl <lucasmelocvl@gmail.com>
2022-05-12 22:07:57 -0300

    Added logical exclusion using softDeletes on Models/Category.php;

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,12 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
+    // Trait que adiciona um comportamento de deleção para realizar uma exclusão lógica, ao invés de física
+    // Em toda a busca, que tiver data no deleted_at, não irá ser mostrada
+    use SoftDeletes;
+
     protected $fillable = [
         'name',
         'description',
         'is_active'
     ];
+
+    protected $dates = ['deleted_at'];
 }
