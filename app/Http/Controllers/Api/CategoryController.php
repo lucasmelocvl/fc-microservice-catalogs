a7d33ccefd3bd3695388b7a52f5cf524c30ee7d4
lucasmelocvl <lucasmelocvl@gmail.com>
2022-05-12 21:46:29 -0300

    Implemented update and delete methods;

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -8,32 +8,52 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+    private $rules = [
+        'name' => 'required|max:255',
+        'is_active' => 'boolean'
+    ];
+
+    /*
+     * Return status code 200 on success
+     */
     public function index()
     {
         return Category::all();
     }
 
-    public function store(Request $request)
+    /*
+     * Return status code 201 (created) on success
+     */
+    public function store(Request $request) //
     {
-        $this->validate($request, [
-            'name' => 'required|max:255',
-            'is_active' => 'boolean'
-        ]);
+        $this->validate($request, $this->rules);
         return Category::create($request->all());
     }
 
-    public function show(Category $category)    // Route Model Binding (ligação do model com a rota)
+    /*
+     * Route Model Binding (ligação do model com a rota)
+     */
+    public function show(Category $category)
     {
         return $category;
     }
 
+    /*
+     * Return status code 200 on success
+     */
     public function update(Request $request, Category $category)
     {
-        //
+        $this->validate($request, $this->rules);
+        $category->update($request->all());
+        return $category;
     }
 
+    /*
+     * Return status code 204 (no content) on success
+     */
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+        return response()->noContent();
     }
 }
