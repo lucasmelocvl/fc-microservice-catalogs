05848ab5ef2fb1c8ac5310f1920047802e7c5377
lucasmelocvl <lucasmelocvl@gmail.com>
2022-05-12 22:49:53 -0300

    Add migration, factory and seed for Gender; Add CRUD for Genders;

--- database/migrations/2022_05_13_012835_create_genders_table.php
+++ database/migrations/2022_05_13_012835_create_genders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCategoriesTable extends Migration
+class CreateGendersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreateCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+        Schema::create('genders', function (Blueprint $table) {
+            $table->uuid('id');
             $table->string('name');
-            $table->text('description')->nullable();
             $table->boolean('is_active')->default(true);
             $table->timestamps();
             $table->softDeletes();
@@ -30,6 +29,6 @@ class CreateCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('genders');
     }
 }
